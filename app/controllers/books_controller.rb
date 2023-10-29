3ae3f206d9b5e14a63ef75bc644307d1615de769
EC2 Default User <ec2-user@ip-172-31-39-71.ap-northeast-1.compute.internal>
2023-10-29 08:17:33 +0000

    [Finish] バリデーション・エラー

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,18 @@ class BooksController < ApplicationController
   
   def create
     @book=Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice]="Book was successfully create."
+      redirect_to book_path(@book.id)
+    else
+      @books=Book.all
+      render :index
+    end
   end
 
   def index
-    @books=Book.all
     @book=Book.new
+    @books=Book.all
   end
 
   def show
@@ -23,8 +28,14 @@ class BooksController < ApplicationController
   
   def update
     book=Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice]="Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      @book=book
+      flash[:alart]="Book was not successfully updated."
+      render :edit
+    end
   end
   
   def destroy
